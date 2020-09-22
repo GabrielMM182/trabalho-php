9af7e91e3f281dfc6b4641b4a63722d6d8687a0c
Gabriel MMorais <gmm_morais@hotmail.com>
2020-09-21 22:34:24 -0300

    add insert e database

--- tabelas.sql
+++ tabelas.sql
@@ -1,3 +1,5 @@
+ CREATE DATABASE chale;
+ 
  CREATE TABLE clientes (
 	 id PRIMARY KEY INTEGER,
 	 nome VARCHAR(200),
@@ -25,3 +27,18 @@ CREATE TABLE quartos (
 	valor VARCHAR(50),
 	statuss VARCHAR(50)
 )
+
+INSERT INTO clientes (id, nome, documento, dataa-nascimento, cidade, estado)
+VALUES (1, gabriel, 00144447, 06-15-1999, pouso alegre, MG);
+
+INSERT INTO clientes (id, nome, documento, dataa-nascimento, cidade, estado)
+VALUES (2, joão, 071558447, 04-14-1997, são paulo, SP);
+
+INSERT INTO reservas (id, id-quarto, id-cliente, dataa-entrada, dataa-saida, valor-reserva, statuss-reserva, dataa-hora)
+VALUES (1, 1, 1, 04-10-2020, 08-10-2020, 6200, checkout, 14:05);
+
+INSERT INTO quartos (id, numero-porta tipo-quarto, valor, statuss)
+VALUES (7, 42, duplo, 420, ocupado);
+
+INSERT INTO quartos (id, numero-porta tipo-quarto, valor, statuss)
+VALUES (10, 07, triplo, 1200, livre);
